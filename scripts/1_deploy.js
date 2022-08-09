f38a4b79a0e3016078a88afb3d9a7342dad3d534
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-08-09 18:41:54 +0700

    Perform smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log(`Token Deployed to: ${token.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
