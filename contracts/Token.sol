e2a1078db834d88397404acc00f92063e7e7c708
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-08-10 00:45:49 +0700

    Implement delegated transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,7 +13,11 @@ contract Token {
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
     constructor(
         string memory _name,
@@ -31,13 +35,7 @@ contract Token {
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
-
+        _transfer(msg.sender, _to, _value);
         return true;
     }
 
@@ -50,5 +48,33 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        _transfer(_from, _to, _value);
+
+        allowance[_from][msg.sender] -= _value;
+
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -85,7 +85,7 @@ describe("Token", () => {
         ).to.be.reverted;
       });
 
-      it("rejects invalid receipent", async () => {
+      it("rejects invalid recepient", async () => {
         const amount = tokens(100);
 
         await expect(
@@ -135,6 +135,65 @@ describe("Token", () => {
             .connect(deployer)
             .approve("0x0000000000000000000000000000000000000000", amount)
         ).to.be.reverted;
+      });
+    });
+  });
+
+  describe("Delegated Tolen Transfers", () => {
+    let amount, transaction, result;
+
+    beforeEach(async () => {
+      amount = tokens(100);
+      transaction = await token
+        .connect(deployer)
+        .approve(exchange.address, amount);
+      result = await transaction.wait();
+    });
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        transaction = await token
+          .connect(exchange)
+          .transferFrom(deployer.address, receiver.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("Transfer balances", async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999900)
+        );
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+      });
+
+      it("resets the allowance", async () => {
+        expect(
+          await token.allowance(deployer.address, exchange.address)
+        ).to.equal(0);
+      });
+
+      it("emits a Transfer event", () => {
+        const event = result.events[0];
+        const args = event.args;
+
+        expect(event.event).to.equal("Transfer");
+        expect(args.from).to.equal(deployer.address);
+        expect(args.to).to.equal(receiver.address);
+        expect(args.value).to.equal(amount);
+      });
+    });
+
+    describe("Failure", async () => {
+      it("Reject insufficient amounts", async () => {
+        // Transfer more tokens than deployer has
+        const invalidAmount = tokens(1000001);
+        await expect(
+          token
+            .connect(exchange)
+            .transferFrom(deployer.address, receiver.address, invalidAmount)
+        ).to.be.rejected;
+        expect(
+          await token.allowance(deployer.address, exchange.address)
+        ).not.equal(0);
       });
     });
   });
